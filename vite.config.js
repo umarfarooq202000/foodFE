3361350ae91e02218f4dd6b8627ffc8846d23ff2
umar farooq <umarfraooq202000@gmail.com>
2024-06-20 19:15:36 +0530

    Configure base path and add deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/ECommerceFoodApp/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
+  base: '/ECommerceFoodApp/', // replace with your repository name
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ command }) => {
+//   const config = {
+//     plugins: [react()],
+//     base: '/',
+//   }
+
+//   if (command !== 'serve') {
+//     config.base = '/ECommerceFoodApp/'
+//   }
+
+//   return config
+// })
