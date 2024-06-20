ad724755dd6ef905e8988b7aaf19d09a2fdf0afa
umar farooq <umarfraooq202000@gmail.com>
2024-06-20 13:02:13 +0530

    canges the command in package.json

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   
-  base: "./ECommerceFoodApp",
+  base: "/ECommerceFoodApp",
 
   plugins: [react()],
 })
